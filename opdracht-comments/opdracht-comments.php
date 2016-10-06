21ae8615eea142bc1409fcd752c41add00179678
Thessa Moerloos <thessa.moerloos@student.kdg.be>
2016-10-06 17:05:15 +0200

    Toevoegen van opdracht comments

---
+++ opdracht-comments/opdracht-comments.php
@@ -0,0 +1,40 @@
+<?php 
+    $naam = "Thessa ";
+    $achternaam = "Moerloos";
+
+/* COMMENTAARBLOK MET  NAAM EN EMAIL
+* Thessa moerloos
+* t.moerloos@hotmail.com
+*/
+
+?>
+
+
+
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Opdracht comments</title>
+        <link rel="stylesheet" href="http://web-backend.local/css/global.css">
+        <link rel="stylesheet" href="http://web-backend.local/css/facade.css">
+        <link rel="stylesheet" href="http://web-backend.local/css/directory.css">
+    </head>
+    <body class="web-backend-opdracht">
+        
+        <section class="body">
+        
+			<h1>Opdracht comments: deel 1</h1>
+
+			<ul>
+				<p>Watashi~no namae~wa <?php echo $naam, $achternaam ?>~desu.</p>
+				
+
+				<p>Mijn commentaarblok staat in mijn php code.</p>
+			</ul>
+			
+		</section>
+
+    </body>
+</html>
