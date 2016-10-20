10fa75359a4fe260939a93cadc911588f73ecbfd
Thessa Moerloos <thessa.moerloos@student.kdg.be>
2016-10-20 16:08:27 +0200

    Toevoegen oef GET nog niet volledig

---
+++ opdracht-get/opdracht-get.php
@@ -0,0 +1,95 @@
+<?php
+
+$artikels = array(	   array("titel" 	=> 	"Lot van Marslander Schiaparelli onduidelijk",
+                              "datum" 	=> 	"19/10/16 - 16u58",
+                              "afbeelding" => "art1.jpg",
+                              "afbeeldingBeschrijving" => "Marslander Schiaparelli",
+                              "inhoud" 	=> 	"Een Europese satelliet is vandaag in een baan rond Mars gekomen. Maar het is niet duidelijk of het is gelukt om een laboratorium op Mars te laten landen. Die lander heeft geen signaal rechtstreeks naar de aarde gestuurd. De vluchtleiding in Duitse stad Darmstadt wachtte er gespannen op, maar tevergeefs.
+Via een omweg kwam er wel een teken van leven, maar de vluchtleiding weet nog niet zeker wat dat teken precies inhoudt en durft nog geen bevestiging te geven.Het uitblijven van het signaal kan betekenen dat de landing is mislukt, maar een andere mogelijkheid is dat het signaal te zwak is. Op een andere manier probeert de ESA alsnog zekerheid te krijgen. In de loop van de avond moet het lot van de lander duidelijk worden.
+De satelliet en de lander zijn in de afgelopen maanden samen naar Mars gevlogen. Afgelopen zondag scheidden hun wegen.
+De lander moest om 16.42 uur Nederlandse tijd de dampkring van Mars binnenkomen met een snelheid van 21.000 kilometer per uur. Wrijving en een parachute moesten hem genoeg afremmen, zodat hij zes minuten later kon neerploffen. Daarna moest hij een levensteken sturen, dat bijna tien minuten later de aarde had moeten bereiken.
+Rond 17.00 uur had dus duidelijk moeten zijn of de landing was geslaagd, maar dat lukte niet. Een oude Europese satelliet bij Mars had wel een teken van de lander opgepikt en dat doorgestuurd naar de aarde.
+De landing van het Schiaparelli-laboratorium is een experiment, bedoeld om de technologie voor zo'n landing te testen. Europa wil rond 2020 een onbemande rijdende verkenner naar Mars brengen. Maar de Schiaparelli heeft ook een paar dagen energie voor wetenschappelijk onderzoek. Zo meet hij windsnelheid, luchtdruk, temperatuur en luchtvochtigheid om hem heen.
+De satelliet is de Trace Gas Orbiter. Die moet in een baan rond Mars gaan zoeken naar gassen in de dampkring. Wetenschappers zijn vooral geïnteresseerd in methaan, een gas dat zou kunnen wijzen op minieme vormen van leven.
+Het is de tweede Europese Marslanding. De eerste poging, van de Beagle 2 in december 2003, mislukte. De verkenner heeft na aankomst nooit iets van zich laten horen. Pas elf jaar later werd hij teruggevonden. Toen bleek dat de zonnepanelen niet waren opengegaan. Daardoor bleef de antenne verborgen en konden de lander en de aarde niet met elkaar communiceren.
+Europa voert de missie uit met Rusland. Het totale project - lander, satelliet en verkenner - kost Europa ruim 1 miljard euro. ",
+                              ),
+
+
+
+                        array( "titel" 	=> 	"Kosmische straling zal astronauten naar Mars dement en paranoïde maken",
+                                "datum" 	=> 	"11/10/16 - 18u07",
+                                "afbeelding" => "art2.jpg",
+                                "afbeeldingBeschrijving" => "astronaut",
+                                "inhoud" 	=> 	" Amerikaans president Obama wil mensen op Mars laten wonen, zo maakte hij vandaag bekend. 'Die reuzensprong zal Amerika maken', liet hij weten. Vrijwilligers genoeg, alleen moeten die weten dat ze door de kosmische straling risico lopen dement en/of paranoïde te worden. Daarvoor waarschuwt een nieuwe studie.
+Astronauten op weg naar Mars kunnen dementie ontwikkelen, naast een onbeheersbaar angstgevoel dat de onderzoekers 'space brain' noemen. Wetenschappers onderzochten de effecten van de kosmische straling waarmee astronauten zullen gebombardeerd worden. Die blijkt een probleem te vormen voor wie een buitenaardse kolonie zou willen stichten op de Rode Planeet.
+NASA onderzoekt momenteel hoe het mensen naar Mars kan sturen, en de Nederlandse groep Mars One plant de eerste bemande Marsvlucht tegen 2027. De Amerikaanse ondernemer Elon Musk wil tegen 2022 mensen naar Mars sturen.
+Angstbestrijdingsproces
+Professor Charles Limoli, expert in stralingsoncologie aan de universiteit van Californië, stelde samen met zijn collega's vast dat deeltjes in die kosmische straling op lange termijn bij muizen voor verminderde hersenfuncties en dementie leiden. De straling bleek ook in te werken op het zogenaamde 'angstbestrijdingsproces', dat de mens helpt angsaanjagende incidenten te overwinnen zodat hij bijvoorbeeld opnieuw kan gaan zwemmen na bijna verdronken te zijn. Dat stelt Limoli in het vakblad Scientific Reports.
+'Dat is geen goed nieuws voor astronauten die een reis van twee tot drie jaar naar Mars aanvatten', besluit hij.
+Levenslang
+'De ruimte stelt de astronauten voor unieke gevaren. Blootstelling aan die stralingspartikels kan leiden tot een hele reeks complicaties in het centrale zenuwstelsel, tijdens de ruimtereis en lang daarna. Het gaat dan om de afname van prestaties, geheugenverlies, angsten, depressies en verzwakte besluitvorming. Die kunnen levenslang aanhouden'.
+Paniek
+De eerste Marsverkenners riskeren ook nog tijdens de vlucht aan paranoïa ten prooi te vallen door het effect op het hersenproces dat afrekent met stresserende gebeurtenissen. 'Die verminderde angstbestrijding maakt je vatbaar voor paniek, wat voor problemen kan zorgen tijdens zo'n lange ruimtereis', stelt Limoli.
+Astronauten hebben dan wel al langer dan een jaar in het International Space Station (ISS) verbleven, daar werden ze niet aan dezelfde mate van kosmische straling blootgesteld omdat ze in de beschermende magnetosfeer van de Aarde zitten.
+'Niet aan te ontsnappen'
+Delen van het ruimtetuig zouden extra schilden tegen kosmische stralen kunnen krijgen, maar dat volstaat vooralsnog niet om de astronauten te beschermen. 'Er valt niet aan de kosmische stralen te ontsnappen', meent Limoli. Hij en zijn team werken aan een medicijn dat bescherming zou bieden tegen de zwaarste effecten van de straling."
+                            ),
+
+                        array( "titel" 	=> 	"NASA beëindigt Marssimulatie van één jaar op Hawaï",
+                                "datum" 	=> 	"29/08/16 - 01u00",
+                                "afbeelding" => "art3.jpg",
+                                "afbeeldingBeschrijving" => "hawai",
+                                "inhoud" 	=> 	" Leven op Mars is dichterbij dan je wellicht denkt: zes mensen hebben afgelopen jaar 'daar' doorgebracht. Maar vandaag kon de crew eindelijk hun ruimtevaartpak uittrekken en terugkeren naar de aarde: na bijna een jaar lang in isolatie geleefd te hebben op Hawaï in een simulatie van de 'rode planeet', heeft NASA het experiment beëindigd.
+De missie was het langste experiment tot nu toe om de gevolgen te meten van het isolement en de beperkingen die een drie jaar durende vlucht naar de rode planeet zich mee zou brengen. Een crew van vier Amerikanen, een Fransman en een Duitser, woonde samen op zo'n 2.500 meter boven zeeniveau in een kleine koepel (diameter: 11 meter diameter, hoogte: 6 meter) op de rotsachtige grond van de vulkaan Mauna Loa-vulkaan op Hawaï: het dichtste bij een Mars leefomgeving dat we op aarde kunnen komen.
+De leefomstandigheden waren zwaar: er was geen frisse lucht, geen vers eten of privacy. Communicatie met de buitenwereld was slechts met een vertraging van 20 minuten (net zoals dat op Mars  zou zijn) mogelijk.
+Beproevingen
+Als de crewleden (een journalist, een bodemspecialist, een astro-bioloog, een natuurkundige, een piloot en een architect) naar buiten wilden gaan, moesten ze zware ruimtepakken aandoen. Om persoonlijke conflicten te vermijden, moesten ze werken en wetenschappelijke experimenten uitvoeren. Om het allemaal nog wat zwaarder te maken, werd de crew onverwachts voor zware 'beproevingen' (variërend van noodsituaties, tot gebroken werkmateriaal)  gesteld. Onderzoekers wilden zo de groepscohesie en -prestaties meten. Daarnaast moesten de 'astronauten' wekelijkse vragenlijsten invullen en werden de 'formele vergaderingen in de keuken opgenomen. Crewleden die last hadden van heimwee konden een virtual reality-spelletje spelen dat ontworpen is om astronauten die lange missies uitvoeren te 'troosten'.
+Zwemmen
+'De onderzoekers kijken vooral uit naar het eten van vers voedsel en ander eten dat ze tijdens het experiment niet kregen. Daarnaast kunnen ze niet wachten om in de oceaan te gaan zwemmen', vertelt Kim Binsted, die de simulatie leidde.
+Reis van drie jaar
+NASA hoopt om tegen 2030 de eerste mensen naar Mars te kunnen sturen. In voorbereiding daarop, houdt de ruimtevaartorganisatie verschillende 'campagnes' of experimenten waarmee ze een zo goed mogelijk beeld probeert te krijgen van wat de eerste crew nodig heeft om de lange reis te maken. Een enkele reis naar Mars duurt namelijk alleen al een slordig jaar. 'Er gaat nooit een 'perfecte' crew zijn', zegt Binsted. 'Maar inzichten uit deze experimenten kunnen wel helpen om een zo'n ideaal mogelijke groep mensen samen te stellen. Er zullen altijd dingen misgaan, maar op basis van het materiaal dat we met deze experimenten hebben verzameld hopen we erachter te komen hoe we de intermenselijke problemen het makkelijkste kunnen tackelen onderweg.'
+Compromissen
+Het derde en langste experiment tot nu toe eindigde vandaag: de crewleden werden met de boodschap 'welkom terug op Aarde' uit hun isolement vrijgelaten. Na de missie raadde de Duitse onderzoekster Christiane Heinicke toekomstige Marsreizigers aan om 'iets mee te nemen om aan te werken: een van de grootste vijanden is de verveling. Je moet bovendien bereid zijn om je aan te passen aan de anderen en om compromissen te maken. Als je dat niet kunt, dan moet je niet naar Mars gaan.'
+Droom
+De missie op Hawaï was de op één na langste ooit, na een Russische missie die 520 dagen duurde. Er werden eerder al drie korte simulaties uitgevoerd op Hawaï, en er zullen in de toekomst nog twee andere missies plaatsvinden, waarvan er één acht maanden zal duren.
+Moest de droom van vele wetenschappers werkelijkheid worden om naar Mars te vliegen, zou de reis naar de Rode Planeet alleen al ongeveer een jaar duren. Experts schatten dat een missie naar Mars één tot drie jaar zou kunnen duren. "
+                              )
+                  );
+
+
+
+
+ ?>
+
+
+
+ <!DOCTYPE html>
+ <html>
+   <head>
+     <meta charset="utf-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1">
+     <title>Opdracht Get</title>
+     <link rel="stylesheet" href="http://web-backend.local/css/global.css">
+     <link rel="stylesheet" href="http://web-backend.local/css/facade.css">
+     <link rel="stylesheet" href="http://web-backend.local/css/directory.css">
+   </head>
+   <body>
+
+<!-- Voor elke keer dat hij door foreach loopt gaat hij elke keer een andere id(en zijn waarde) aanspreken  -->
+     <?php foreach ($artikels as $id => $value): ?>
+
+            <h1><?php echo $value["titel"] ?></h1>
+
+            <img src="img/<?php echo $value["afbeelding"] ?>" alt="<?php echo $value["afbeeldingBeschrijving"] ?>" />
+
+            <p> <?php echo substr( $value["inhoud"], 0, 50) . "..." ?></p>
+
+            <a href="opdracht-get.php?id=<?php echo $id ?>">Lees meer >></a>
+
+
+     <?php endforeach; ?>
+
+
+   </body>
+ </html>
