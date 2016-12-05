388ab286a715017adb9d2461ebc84b19edffb5ed
Thessa Moerloos <thessa.moerloos@student.kdg.be>
2016-12-05 16:11:14 +0100

    Toevoegen opdracht include-require

---
+++ opdracht-include-require/index.php
@@ -0,0 +1,27 @@
+<?php
+
+$artikels = array( array("titel"	=> 	"PHP 8.4 gereleased",
+                          "text" => 	"De nieuwste versie van PHP bevat uitbreidingen op oa. mind control-facades,
+                                       in-browser 3d projecties en de API voor drones.",
+                          "tags" => 	"Kernwoorden: new, PHP 8.4"
+                        ),
+
+                   array("titel"	=> 	"Zymphovel framework",
+                          "text" => 	"Wordt Zymphovel het nieuwste PHP-framework dat de wereld in een sneltempo zal veroveren?",
+                          "tags" => 	"Kernwoorden: frameworks"
+                        ),
+
+                   array("titel"	=> 	"Rasmus Lerdorf vermist",
+                         "text"  => 	"De geestelijke vader van PHP Rasmus Lerdorf is sinds vorige week vermist
+                                       nadat hij met zijn privé-jet richting Ibiza vertrok.",
+                         "tags"  => 	"Kernwoorden: bizar, rasmus lerdorf, feestneus"
+                         )
+                 );
+
+ ?>
+
+ <?php include'view/header-partial.html' ?>
+
+  <?php include'view/body-partial.html' ?>
+
+  <?php include'view/footer-partial.html' ?>
---
+++ opdracht-include-require/opdracht-include-require-html.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Include/require HTML</title>
+    <link rel="stylesheet" href="http://web-backend.local/css/global.css">
+    <link rel="stylesheet" href="http://web-backend.local/css/facade.css">
+    <link rel="stylesheet" href="http://web-backend.local/css/directory.css">
+  </head>
+
+  <body>
+
+    <div class="facade-minimal opdracht-include-require" data-url="http://www.app.local/index.php">
+
+
+      <div>Welkom op deze nieuwssite</div>
+
+        <section class="Nieuws">
+            <h1>Nieuws</h1>
+
+            <article>
+                <h1>PHP 8.4 gereleased</h1>
+                <p>De nieuwste versie van PHP bevat uitbreidingen op oa. mind control-facades, in-browser 3d projecties en de API voor drones.</p>
+                <p>Kernwoorden: new, PHP 8.4</p>
+
+            </article>
+
+            <article>
+                <h1>Zymphovel framework</h1>
+                <p>Wordt Zymphovel het nieuwste PHP-framework dat de wereld in een sneltempo zal veroveren?</p>
+                <p>Kernwoorden: frameworks</p>
+            </article>
+
+            <article>
+                <h1>Rasmus Lerdorf vermist</h1>
+                <p>De geestelijke vader van PHP Rasmus Lerdorf is sinds vorige week vermist nadat hij met zijn privé-jet richting Ibiza vertrok.</p>
+                <p>Kernwoorden: bizar, rasmus lerdorf, feestneus</p>
+            </article>
+        </section>
+
+        <footer>
+            Daily PHP is een blog over PHP.
+        </footer>
+
+    </div>
+
+
+
+
+  </body>
+</html>
